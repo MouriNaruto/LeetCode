5b9ed5092bda4d7e7880085fdb1ff3a0f85d17a7
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-01-10 11:09:43 +0800

    Create the LeetCode project.

---
+++ LeetCode/LeetCode.cpp
@@ -0,0 +1,19 @@
+﻿/*
+ * PROJECT:   LeetCode
+ * FILE:      LeetCode.cpp
+ * PURPOSE:   Implementation for LeetCode
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include <cstdio>
+
+int main()
+{
+    std::printf("Hello World!\n");
+    std::getchar();
+
+    return 0;
+}
